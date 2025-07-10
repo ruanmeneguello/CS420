efc1b9d20e24724c92e5b980846c9c3bfb5c4b09
ruanmeneguello <ruan.meneguello@gmail.com>
2025-07-09 19:09:55 -0600

    Remove all files to empty the repository

--- src/main/java/com/getsimplex/steptimer/datarepository/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.getsimplex.steptimer.datarepository;
-
-import com.getsimplex.steptimer.model.Customer;
-
-public class CustomerRepository extends GenericRepository<Customer>{//using an abstract class so we can infer the type more easily using getGenericSuperClass() method
-}
--- src/main/java/com/getsimplex/steptimer/datarepository/RapidStepTestRepository.java
+++
@@ -1,6 +0,0 @@
-package com.getsimplex.steptimer.datarepository;
-
-import com.getsimplex.steptimer.model.RapidStepTest;
-
-public class RapidStepTestRepository extends GenericRepository<RapidStepTest>{//using an abstract class so we can infer the type more easily using getGenericSuperClass() method
-}
